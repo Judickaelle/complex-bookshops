620171ead90f97cfdb5bc8bcdd262b7423686c79
Judickaelle <judickaelle.pelletier@gmail.com>
2022-01-17 11:54:47 +0100

    test et debut

---
+++ bookshop/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="counter">
+        {{ count }}
+        <div class="counter_button" @click="increment"> + </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment () {
+                this.count++
+            }
+        }
+    }
+</script>
+
+<style>
+    .counter{
+        font-weight: blod;
+        width: 20 px;
+        height: 20 px;
+        text-align: center;
+        background-color: rgb(180, 180, 180);
+    }
+
+    .counter_button{
+        padding: 10px;
+        border: 1px solid black;
+    }
+</style>
---
+++ bookshop/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Bookshop</b-navbar-brand>
+                <!--search form-->
+               <b-form class="inline">
+                    <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+                    <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-form>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    components: {
+    },
+    computed: {
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+    .inline{
+        display: flex;
+        width: auto;
+        //faire un alignement à droite 
+    }
+</style>
